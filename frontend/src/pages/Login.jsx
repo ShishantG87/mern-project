f713f6072bf8f9144c9dd4fbe4ca3d8e2cccb966
Shishant Bohora <shishantbohora44@gmail.com>
2025-10-20 17:59:16 -0400

    Update API endpoint for user login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${BACKEND_URL}/users/me`, formData);
+      const res = await axios.post(`${BACKEND_URL}/users/login`, formData);
       localStorage.setItem("token", res.data.token);
       console.log(res.data)
       setUser(res.data);
