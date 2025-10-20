7f5f48361ec2fc54fecba23c8266a04b3dcb12e0
ShishantG87 <shishantbohora44@gmail.com>
2025-10-20 17:29:37 -0400

    done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.js';
 import { connectDB } from  "./config/db.js";
-
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,7 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors({
+    origin: "https://mern-project-frontend-9djz.onrender.com/",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
 app.use(express.json());
+
 
 
 app.use("/api/users", authRoutes);
